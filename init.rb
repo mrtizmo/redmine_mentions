3e016f2e08ef727d8717ef7144f7803ee824b456
Andrei Malyshev <Andrei.Malyshev@kodep.ru>
2019-07-23 17:57:25 +0300

    Fixed multiline mentions and users without preference

--- init.rb
+++ init.rb
@@ -2,7 +2,7 @@ Redmine::Plugin.register :redmine_mentions do
   name 'Redmine Mentions plugin'
   author 'RedmineUP'
   description 'Adds mention support for issue notes'
-  version '0.0.1'
+  version '0.0.2'
   url 'https://redmineup.com'
   author_url 'https://redmineup.com'
   requires_redmine version_or_higher: '3.4.0'
